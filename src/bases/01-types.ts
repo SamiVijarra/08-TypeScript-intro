f87203239cd6b3247bf58d5c6f5f55d13316df52
Sami <samivijarra1@gmail.com>
2026-04-22 16:56:51 -0300

    feat: add data types

--- src/bases/01-types.ts
+++ src/bases/01-types.ts
@@ -1,3 +1,17 @@
 export const name: string = 'TypeScript'; 
 export const age: number = 35;
 export const isValid: boolean = true;
+
+export const templateString: string = 
+`Esto es un string
+multilinea 
+que puede tener 
+" dobles
+' simples 
+inyectar valores ${name}
+expresiones ${ 1 + 1}
+numeros ${age}
+booleanos ${isValid}
+`;
+
+console.log(templateString);
